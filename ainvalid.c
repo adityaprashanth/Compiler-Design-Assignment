511fc6ee485e462ec88319b7224173d6240bfc9c
Aditya Prashanth <62784469+adityaprashanth@users.noreply.github.com>
2025-06-01 20:37:40 +0530

    Add files via upload

---
+++ ainvalid.c
@@ -0,0 +1,76 @@
+int main()
+{
+	int a, b;
+	a = 10;
+	b = 11;
+	a = 8997 / 5816 / 425									//semi-colon missing
+
+	if(a <= b)
+	{
+		int a4;
+		a4 = 2416 + 4649;
+	}
+	else (a > b)													//no condition allowed for else block
+	{
+		int a3;
+		a3 = 3916 + 3698 - 3684;
+	}
+	else
+	{
+		int a6;
+		a6 = 5067 * 3179 / 3287;
+	}
+	while(a < b){
+		a = 10;
+	}
+	while(a <= b)
+	{
+		while(a <= b)
+		{
+			while(a >= b):										//no colon required for while
+			{
+				int a3;
+				a3 = 3323 == 2665 + 297 > 5816;
+				int a4;
+				a4 = 6423 + 3661 * 1998;
+																//while block not closed
+		}
+	}
+
+	int -abcs;									// variable name cannot start with -
+	int 1abc;									// variable name cannot start with a digit
+	int double;									// keywords cannot be used as variable names
+	double = 100;				 				
+	if(a < b):									
+	{
+		a = 10;									
+		c = 10;
+	}
+	if a >= a 									// condition should be in parentheses
+	{
+		a = 4436 + 2045 - 5360 * 8997;
+	}
+	if(a > b)
+	{
+		if(a < b)
+		{
+			if(a == b)
+			{
+				int a5;
+				a5 = 7876 * 1661 +* 146;		//+* is not a valid operator
+			}								//else block missing
+            else 
+		}
+	}
+	while(a <= b){
+		while(a <= b){
+			while(a >= b)
+			{
+				int a3;
+				a3 = 3323 == 2665 + 297 > 5816;
+				int a4;
+				a4 = 6423 + 3661 * 1998 * 9083 > 2841;
+			}
+		}
+	}
+}
---
+++ avalid.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    int a = 10;
+    int d;
+    int c = 10;
+    
+
+    printf("Hello World\n%d", 10);
+
+    if (d > 0){
+        printf("Positive\n");
+    }
+    else{
+        printf("Zero\n");
+    }
+    int aaa=1;
+    int bbb=++aaa;
+
+    int i;
+    for (i=0; i<10; i++){
+        int a = 10;
+    }
+    int arr[2][3];
+    int arr1[5];
+    int arr2[2][3][4];
+
+    if(n == 0){
+        return 1;
+    }
+    return n * fact(n-1);
+}
